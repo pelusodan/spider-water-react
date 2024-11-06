cd0c91f26b49dd406601a1d2451817f9db1856c4
Dan <pelusodan@gmail.com>
2024-11-06 00:44:42 -0500

    adding floating head and main page basic styling

--- src/main-page/MainPage.js
+++ src/main-page/MainPage.js
@@ -1,38 +1,23 @@
 import React, { useState, useEffect } from 'react';
-import './MainPage.css'; // CSS file for animations
-
-// Import images
-import Image1 from '../assets/images/chuck.png';
-import Image2 from '../assets/images/dan.png';
+import './MainPage.css';
+import Image1 from '../assets/images/dan.png';
+import Image2 from '../assets/images/stin.png';
 import Image3 from '../assets/images/emma.png';
-import Image4 from '../assets/images/stin.png';
+import Image4 from '../assets/images/chuck.png';
+import { Typewriter } from 'react-simple-typewriter';
+
 
 const images = [Image1, Image2, Image3, Image4];
 
 const MainPage = () => {
   const [currentImage, setCurrentImage] = useState(0);
-  const [text, setText] = useState('');
-  const titleText = "spider water";
 
   // Rotate images every 5 seconds
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentImage((prevImage) => (prevImage + 1) % images.length);
     }, 5000);
-
     return () => clearInterval(interval);
-  }, []);
-
-  // Typing animation effect
-  useEffect(() => {
-    let i = 0;
-    const typeInterval = setInterval(() => {
-      setText(titleText.slice(0, i + 1));
-      i++;
-      if (i === titleText.length) clearInterval(typeInterval);
-    }, 150);
-
-    return () => clearInterval(typeInterval);
   }, []);
 
   return (
@@ -40,7 +25,16 @@ const MainPage = () => {
       <div className="image-container">
         <img src={images[currentImage]} alt="Rotating" className="floating-image" />
       </div>
-      <h1 className="typing-text">{text}</h1>
+      <h1 className="typing-text">
+        <Typewriter
+          words={['spider water']}
+          loop={1}
+          typeSpeed={120}
+          deleteSpeed={50}
+          cursor
+          cursorStyle="_"
+        />
+      </h1>
       <p className="location-label">cambridge ma - cryptid pop</p>
     </div>
   );
